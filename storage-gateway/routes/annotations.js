da0d80d550c8f08bcec8772802c3287c68fe7a1a
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-05-13 14:25:16 +0200

    Added unirest dependency and updted indentation

--- storage-gateway/routes/annotations.js
+++ storage-gateway/routes/annotations.js
@@ -1,46 +1,49 @@
+"use strict";
+
 const express = require('express');
 const router = express.Router();
+const unirest = require('unirest');
 
 var myAnno;
 
 router.route('/')
-	.get((request, response) => {
-		// index
-		if (myAnno) {
-			console.log("Annotation found!");
-			response.json([ myAnno ]);
-		} else {
-			console.log("No annotation found!");
-			response.json([]);
-		}
-	})
-	.post((request, response) => {
-		// create
-		myAnno = request.body;
-		myAnno.id = "4711";
-		response.status(303)
-				.location("/annotations/" + myAnno.id)
-				.end();
-	});
+    .get((request, response) => {
+        // index
+        if (myAnno) {
+            console.log("Annotation found!");
+            response.json([myAnno]);
+        } else {
+            console.log("No annotation found!");
+            response.json([]);
+        }
+    })
+    .post((request, response) => {
+        // create
+        myAnno = request.body;
+        myAnno.id = "4711";
+        response.status(303)
+            .location("/annotations/" + myAnno.id)
+            .end();
+    });
 
 router.route('/:id')
-	.get((request, response) => {
-		// read
-		response.json(myAnno);
-	})
-	.put((request, response) => {
-		// update
-		myAnno = request.body;
-		myAnno.id = "4711";
-		response.status(303)
-				.location("/annotations/" + myAnno.id)
-				.end();
-	})
-	.delete((request, response) => {
-		// delete
-		myAnno = null;
-		response.status(204)
-				.end();
-	});
+    .get((request, response) => {
+        // read
+        response.json(myAnno);
+    })
+    .put((request, response) => {
+        // update
+        myAnno = request.body;
+        myAnno.id = "4711";
+        response.status(303)
+            .location("/annotations/" + myAnno.id)
+            .end();
+    })
+    .delete((request, response) => {
+        // delete
+        myAnno = null;
+        response.status(204)
+            .end();
+    });
 
 module.exports = router;
--- storage-gateway/server.js
+++ storage-gateway/server.js
@@ -11,14 +11,14 @@ const annotations = require('./routes/annotations');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use((request, response, next) => {
-	response.header("Access-Control-Allow-Origin", "*");
-	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	response.header("Access-Control-Allow-Methods", "DELETE, GET, POST, PUT");
-	next();
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header("Access-Control-Allow-Methods", "DELETE, GET, POST, PUT");
+    next();
 });
 
 app.use('/', index);
@@ -28,16 +28,16 @@ app.use('/annotations', annotations);
  * Handle not found error
  */
 app.use((request, response, next) => {
-	let error = new Error('Resource not found');
-	error.status = 404;
-	next(error);
+    let error = new Error('Resource not found');
+    error.status = 404;
+    next(error);
 });
 
 app.use((error, request, response, next) => {
-	response.status(error.status || 500)
-			.json( { "error_message": error.message || "Unknown error" } );
+    response.status(error.status || 500)
+        .json({"error_message": error.message || "Unknown error"});
 });
 
-app.listen(52629, function() {
-	console.log("Server started...");
+app.listen(52629, function () {
+    console.log("Server started...");
 });
