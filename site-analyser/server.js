b35e634870d47b02226576d5883e5f83b421ebc4
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-05-27 15:43:12 +0200

    Moved site-analyser into a docker container

--- site-analyser/server.js
+++ site-analyser/server.js
@@ -1,72 +1,135 @@
 "use strict";
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const logger = require('morgan');
+
 const unirest = require('unirest');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const xpath = require("simple-xpath-position");
-const annotationFunctions = require("./site-analyser.conf.js")
+const annotationFunctions = require("/conf/site-analyser.conf.js");
 
-function createAnnotation(document, cssSelector, contentType) {
-    const promises = [];
-    document.querySelectorAll(cssSelector)
-        .forEach((node) => {
-            const nodeXpath = xpath.fromNode(node, document.body);
+const app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
-            // TODO: If we trim the textContent, not the whole text in the node is selected
-            const nodeTextContent = node.textContent;
-
-            const annotation = {
-                "annotator_schema_version": "v1.0",
-                "text": contentType,
-                "quote": nodeTextContent,
-                "uri": "http://localhost",
-                // TODO: Text is not completely selected!
-                "ranges": [
-                    {
-                        "start": nodeXpath,
-                        "end": nodeXpath,
-                        "startOffset": 0,
-                        "endOffset": nodeTextContent.length
-                    }
-                ],
-                "permissions": {
-                    "read": [],
-                    "admin": ["technicalUser"],
-                    "update": ["technicalUser"],
-                    "delete": ["technicalUser"]
+function writeAnnotation(annotation) {
+    return new Promise((resolve, reject) => {
+        unirest.post("http://storage-gateway:52629/annotations")
+            .followRedirect(false)
+            .type("json")
+            .send(annotation)
+            .end((response) => {
+                if (response.status === 303) {
+                    resolve("Annotation successfully written!");
+                } else {
+                    console.log("Could not write...");
+                    reject("Annotation not written: " + response.error);
                 }
-            };
-
-            promises.push(new Promise((resolve, reject) => {
-                unirest.post("http://localhost:52629/annotations")
-                    .followRedirect(false)
-                    .type("json")
-                    .send(annotation)
-                    .end((response) => {
-                        if (response.status === 303) {
-                            resolve("Annotation successfully written!");
-                        } else {
-                            reject("Annotation not written: " + response.error);
-                        }
-                    });
-            }));
-        });
-    return promises;
+            });
+    });
 }
 
-unirest.get("http://localhost")
-   .end((response) => {
-       if (response.ok) {
-           const dom = new JSDOM(response.body);
-           const document = dom.window.document;
+function createAnnotation(document, cssSelector, contentType) {
+    return new Promise((resolve, reject) => {
+        const promises = [];
+        document
+            .querySelectorAll(cssSelector)
+            .forEach((node) => {
+                const nodeXpath = xpath.fromNode(node, document.body);
 
-		   const functions = annotationFunctions.getFunctions();
+                // TODO: If we trim the textContent, not the whole text in the node is selected
+                const nodeTextContent = node.textContent;
 
-		   functions.forEach(annoFun => {
-			   Promise.all(annoFun(document, createAnnotation))
-			   .then(values => values.forEach(value => console.log(value)), error => console.log(error));
-		   });
-       } else {
-           console.log("Could not fetch site.")
-       }
-   });
+                const annotation = {
+                    "annotator_schema_version": "v1.0",
+                    "text": contentType,
+                    "quote": nodeTextContent,
+                    "uri": "http://localhost",
+                    // TODO: Text is not completely selected!
+                    "ranges": [
+                        {
+                            "start": nodeXpath,
+                            "end": nodeXpath,
+                            "startOffset": 0,
+                            "endOffset": nodeTextContent.length
+                        }
+                    ],
+                    "permissions": {
+                        "read": [],
+                        "admin": ["technicalUser"],
+                        "update": ["technicalUser"],
+                        "delete": ["technicalUser"]
+                    }
+                };
+
+                promises.push(writeAnnotation(annotation));
+            });
+
+        Promise
+            .all(promises)
+            .then(() => {
+                resolve(promises.length + " annotations written for '" + cssSelector + "'.");
+            }, reason => {
+                reject("Writing annotation for " + cssSelector + " failed: " + reason);
+            });
+    });
+}
+
+app.post("/analyse", (request, response) => {
+    let getSite = () => {
+        return new Promise((resolve, reject) => {
+            unirest
+                .get("http://test-page")
+                .end((uniRestResponse) => {
+                    if (uniRestResponse.ok) {
+                        resolve(uniRestResponse.body);
+                    } else {
+                        reject({"status": uniRestResponse.status, "error": uniRestResponse.error});
+                    }
+                });
+        });
+    };
+
+    let createAnnotations = (body) => {
+        return new Promise((resolve, reject) => {
+            const dom = new JSDOM(body);
+            const document = dom.window.document;
+            const functions = annotationFunctions.getFunctions();
+
+            const promises = [];
+            functions.forEach(annoFunc => {
+                promises.push(annoFunc(document, createAnnotation));
+            });
+
+            Promise
+                .all(promises)
+                .then(result => {
+                    resolve("All annotations successfully written.");
+                }, reason => {
+                    reject("Not all annotations could be written: " + reason);
+                });
+        });
+    };
+
+    getSite()
+        .then(createAnnotations)
+        .then(() => {
+            console.log("Everything is fine!");
+            response.status(201).end();
+        })
+        .catch(reason => {
+            console.log("Error: " + reason);
+            response
+                .status(500)
+                .json({ "error": reason });
+        });
+});
+
+app.listen(44284, function () {
+    console.log("Analyser service started...");
+});
